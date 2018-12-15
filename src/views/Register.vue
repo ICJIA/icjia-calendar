0ed5f9bee977881a054c10c315c6b11a60648291
Chris Schweda <cschweda@gmail.com>
2018-12-15 16:02:46 -0600

    fix: replace registration page with registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,51 +1,170 @@
 <template>
-  <v-container fill-height>
-    <v-layout row class="text-xs-center" align-center justify-center>
-      <v-flex xs12 sm6 class="grey lighten-4 animated zoomIn fast">
-        <v-card class>
-          <div class="text-xs-center pt-3 pb-3" style="background: #1A237E; color: #fff">
-            <img src="/logo.png" alt="Illinois Criminal Justice Informtion Authority Login">
-            <div style="color: #fff; font-weight: bold; font-size: 16px" class="mt-2">REGISTER</div>
-          </div>
-
-          <div class="pr-3 pl-3 pt-3 pb-3">
-            <v-form @submit="login" onSubmit="return false;">
+  <div class="page-height">
+    <v-container fill-height class="px-3 mt-5" id="page-content">
+      <v-layout row wrap>
+        <v-flex xs10 offset-xs1 style="margin-top: 50px">
+          <div>
+            <form style="margin-top: 0px">
               <v-text-field
-                prepend-icon="person"
-                name="identifier"
-                label="username or email"
-                v-model="identifier"
-                autocomplete="identifier"
-                ref="identifier"
-                @keydown.enter="login"
+                v-model="email"
+                :error-messages="emailErrors"
+                label="Valid @illinois.gov email"
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+                aria-label="Email"
               ></v-text-field>
+
               <v-text-field
-                prepend-icon="lock"
-                name="password"
-                label="password"
-                type="password"
                 v-model="password"
-                autocomplete="password"
-                @keydown.enter="login"
+                :error-messages="passwordErrors"
+                label="Password"
+                :append-icon="e3 ? 'visibility' : 'visibility_off'"
+                @click:append="() => (e3 = !e3)"
+                :type="e3 ? 'password' : 'text'"
+                @input="$v.password.$touch()"
+                @blur="$v.password.$touch()"
+                aria-label="Password"
+                class="mt-2"
               ></v-text-field>
-
-              <v-card-actions class="pt-5">
-                <v-btn primary large block @click="login">Register&nbsp;</v-btn>
-              </v-card-actions>
+              <v-text-field
+                v-model="repeatPassword"
+                :error-messages="repeatPasswordErrors"
+                label="Verify Password"
+                :append-icon="e4 ? 'visibility' : 'visibility_off'"
+                @click:append="() => (e4 = !e4)"
+                :type="e4 ? 'password' : 'text'"
+                aria-label="Verify Password"
+                @input="$v.repeatPassword.$touch()"
+                @blur="$v.repeatPassword.$touch()"
+                class="mt-2"
+              ></v-text-field>
               <div
-                style="font-size: 18px; height: 50px; font-weight: bold"
-                class="mt-3"
+                class="mt-3 text-xs-center"
+                style="height: 50px; font-weight: bold"
               >{{this.$store.state.status}}</div>
-            </v-form>
+              <div v-if="!disabled" class="text-xs-center">
+                <v-btn @click="submit">submit</v-btn>
+              </div>
+              <v-else class="text-xs-center">
+                <v-btn to="/login">Log in</v-btn>
+              </v-else>
+            </form>
+
+            <!-- <tree-view :data="this.$v" :options="{maxDepth: 3}"></tree-view> -->
           </div>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  minLength,
+  alphaNum,
+  sameAs,
+  email
+} from "vuelidate/lib/validators";
+import passwordComplexity from "@/validators/passwordComplexity";
+import illinoisDotGov from "@/validators/illinoisDotGov";
+import axios from "axios";
+import config from "@/config";
+export default {
+  mixins: [validationMixin],
+
+  components: {},
+
+  mounted() {},
+
+  validations: {
+    email: { required, email, illinoisDotGov },
+
+    password: {
+      required,
+      minLength: minLength(8),
+      passwordComplexity
+    },
+    repeatPassword: {
+      sameAsPassword: sameAs("password")
+    }
+  },
+  data() {
+    return {
+      name: "",
+      e3: true,
+      e4: true,
+      email: "",
+      password: "",
+      repeatPassword: "",
+      username: "",
+      showSubmit: true,
+      showAxiosError: false,
+      axiosError: "",
+      showLoader: false,
+      successMessage: "",
+      disabled: false
+    };
+  },
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.required && errors.push("Email is required.");
+      !this.$v.email.email && errors.push("Not a valid e-mail address.");
+      !this.$v.email.illinoisDotGov &&
+        errors.push("You must use a valid @illinois.gov email adddress.");
+      return errors;
+    },
+
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.minLength &&
+        errors.push("Password must have minimum 8 characters.");
+      !this.$v.password.required && errors.push("Password is required.");
+
+      !this.$v.password.passwordComplexity &&
+        errors.push(
+          "Weak password. Please provide at least one uppercase letter and one number. "
+        );
+      return errors;
+    },
+    repeatPasswordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.repeatPassword.sameAsPassword &&
+        errors.push("Passwords must match");
+      return errors;
+    },
+
+    isSuccess(v) {
+      return !this.$v.$invalid && this.$v.$dirty;
+    }
+  },
+  methods: {
+    submit() {
+      this.$v.$touch();
+
+      if (this.isSuccess) {
+        this.showLoader = true;
+        let payload = {
+          username: this.email.toLowerCase(),
+          email: this.email.toLowerCase(),
+          password: this.password
+        };
+        this.$store
+          .dispatch("register", payload)
+          .then(() => {
+            console.log("Success!");
+            this.disabled = true;
+          })
+          .catch(err => console.log(JSON.striginfy(err)));
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
