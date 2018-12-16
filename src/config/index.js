55f96ef9db0b3f572b8ddf018e3cec6a0ed2708a
Chris Schweda <cschweda@gmail.com>
2018-12-16 09:30:48 -0600

    fix: adjust category order

--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,14 @@
 const config = {
   categories: [
-    { name: "ICJIA General", color: "#E91E63" },
-
-    {
-      name: "Site Visits",
-      color: "#6A1B9A"
-    },
     { name: "NOFO release or close dates", color: "#0D47A1" },
     { name: "Grant start dates", color: "#4DB6AC" },
     { name: "Federal application due dates", color: "#1B5E20" },
     { name: "Federal reporting due dates", color: "#EC407A" },
     { name: "Budget Committee dates", color: "#4A148C" },
     { name: "OFM Grantee Audits", color: "#2962FF" },
-    { name: "Out of Office", color: "#B71C1C" }
+    { name: "Site Visits", color: "#6A1B9A" },
+    { name: "Out of Office", color: "#B71C1C" },
+    { name: "ICJIA General", color: "#E91E63" }
   ],
   theme: {
     light: {
